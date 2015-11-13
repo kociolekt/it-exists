8d48d17259e408cccfd24e14337f07fd8d1dcbd8
Tomek Fijoł <tomekfijol@gmail.com>
2015-11-13 18:24:15 +0100

    compact

--- index.js
+++ index.js
@@ -1,50 +1,31 @@
 var fs = require('fs');
 
 function itExsist(someModule) {
-
   try {
-
     return require(someModule);
-
   } catch (e) {
-
     // undefined
     return;
-
   }
-
-};
+}
 
 itExsist.pathSync = function(path) {
-
   try {
-
     return fs.statSync(path);
- 
   } catch (e) {
-
     // undefined
     return;
-
   }
-
 };
 
 itExsist.pathAsync = function(path, cb) {
-
   fs.stat(path, function(err, stats) {
-
     if (err) {
-
       // undefined
       cb();
-
     }
-
     cb(stats);
-
-  })
-
+  });
 };
 
 module.exports = itExsist;
